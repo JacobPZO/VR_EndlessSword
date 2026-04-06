158548123e8ce84495fae2b406c51eced7f59331
JacobPZO <156956701+JacobPZO@users.noreply.github.com>
2026-04-06 15:54:55 -0700

    created script for velocity tracking

---
+++ EndlessSword/Assets/Scripts/PlayerSword.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.Labs.SuperScience
+{
+
+    public class PlayerSword : MonoBehaviour
+    {
+        [SerializeField]
+        //The object that PhysicsTracer will get physics data from
+        public Transform m_ToTrack;
+
+        PhysicsTracker m_MotionData = new PhysicsTracker();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            var position = m_ToTrack.position;
+            m_MotionData.Reset(position, m_ToTrack.rotation, Vector3.zero, Vector3.zero);
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            var position = m_ToTrack.position;
+            m_MotionData.Update(position, m_ToTrack.rotation, Time.smoothDeltaTime);
+            UnityEngine.Debug.Log(m_MotionData.Velocity.ToString());
+        }
+    }
+}
