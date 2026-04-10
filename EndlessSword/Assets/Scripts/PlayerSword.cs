67a4d0516803cdffd379c7263fdacd7b71d4d90d
JacobPZO <156956701+JacobPZO@users.noreply.github.com>
2026-04-09 17:30:30 -0700

    physics scripting

--- EndlessSword/Assets/Scripts/PlayerSword.cs
+++ EndlessSword/Assets/Scripts/PlayerSword.cs
@@ -2,26 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class PlayerSword : MonoBehaviour
-    {
-        [SerializeField]
-        //The object that PhysicsTracer will get physics data from
-        public Transform m_ToTrack;
-
-        PhysicsTracker m_MotionData = new PhysicsTracker();
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            var position = m_ToTrack.position;
-            m_MotionData.Reset(position, m_ToTrack.rotation, Vector3.zero, Vector3.zero);
-        }
-
-        // Update is called once per frame
-        void FixedUpdate()
-        {
-            var position = m_ToTrack.position;
-            m_MotionData.Update(position, m_ToTrack.rotation, Time.smoothDeltaTime);
-            UnityEngine.Debug.Log(m_MotionData.Velocity.ToString());
-        }
-    }
+public class PlayerSword : Sword
+{
+    public Collision Collision;
+}
---
+++ EndlessSword/Assets/Scripts/Sword.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    [SerializeField]
+    //The object that PhysicsTracer will get physics data from
+    public Transform m_ToTrack;
+
+    PhysicsTracker m_MotionData = new PhysicsTracker();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var position = m_ToTrack.position;
+        m_MotionData.Reset(position, m_ToTrack.rotation, Vector3.zero, Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var position = m_ToTrack.position;
+        m_MotionData.Update(position, m_ToTrack.rotation, Time.smoothDeltaTime);
+        //UnityEngine.Debug.Log(m_MotionData.Direction.ToString());
+    }
+
+    Vector3 CompareDirection(Sword swordToCompare)
+    {
+        Vector3 comparedDirection = m_MotionData.Direction - swordToCompare.m_MotionData.Direction;
+        return comparedDirection;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Sword>(out Sword externalSword))
+        {
+            UnityEngine.Debug.Log(CompareDirection(externalSword));
+        }
+    }
+}
